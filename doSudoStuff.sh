a75c18782a86458d5a826fa59e11861466a27bb5
Dominik Businger <dominik@azine-it.ch>
2013-05-09 18:16:28 +0200

    fixed repository base dir

--- doSudoStuff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-echo "do sudo stuff"
-sudo rm app/cache/* -rf
-sudo chmod 777 app/cache -R 
-sudo chmod 777 app/logs -R
-#sudo chmod 777 app/testMails.spool -R
-#sudo chmod 777 vendor/ezyang/htmlpurifier/library/HTMLPurifier/DefinitionCache -R
-
