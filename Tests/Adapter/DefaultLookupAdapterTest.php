eb6ca014f5a31ef5ec23233a79b0070a5f5225e9
dominik <dominik.businger@azine-it.ch>
2013-11-20 17:47:08 +0100

    increase test-coverage, solve dependency problem for travis

--- Tests/Adapter/DefaultLookupAdapterTest.php
+++ Tests/Adapter/DefaultLookupAdapterTest.php
@@ -6,15 +6,30 @@ use Azine\GeoBlockingBundle\Adapter\DefaultLookupAdapter;
 class DefaultLookupAdapterTest extends \PHPUnit_Framework_TestCase{
 
     public function testGetCountry(){
-		$adapter = new DefaultLookupAdapter();
+    	if(function_exists("geoip_country_code_by_name")){
+			$adapter = new DefaultLookupAdapter();
 
+	    	$chIp = "194.150.248.201";
+	    	$this->assertEquals("CH",$adapter->getCountry($chIp), "$chIp should be in Switzerland => CH");
 
-    	$chIp = "194.150.248.201";
-    	$this->assertEquals("CH",$adapter->getCountry($chIp), "$chIp should be in Switzerland => CH");
+	    	$usIp = "8.8.8.8";
+	    	$this->assertEquals("US",$adapter->getCountry($usIp), "$usIp should be in the USA => US");
+    	} else {
+    		$this->markTestSkipped("php geoip-module seems not to be installed.");
+    	}
+    }
 
-    	$usIp = "8.8.8.8";
-    	$this->assertEquals("US",$adapter->getCountry($usIp), "$usIp should be in the USA => US");
-
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testGetCountryWithoutGeoIpModule(){
+    	if(!function_exists("geoip_country_code_by_name")){
+	    	$adapter = new DefaultLookupAdapter();
+	    	$chIp = "194.150.248.201";
+	    	$this->assertEquals("CH",$adapter->getCountry($chIp), "$chIp should be in Switzerland => CH");
+    	} else {
+    		throw new \InvalidArgumentException("It seems, the geo-ip extension is installed, so this test doesn't make sense.");
+    	}
 
     }
 }
--- Tests/Adapter/MaxmindLookupAdapterTest.php
+++ Tests/Adapter/MaxmindLookupAdapterTest.php
@@ -16,11 +16,15 @@ class MaxmindLookupAdapterTest extends \PHPUnit_Framework_TestCase{
 	}
 
      public function testGetCountryMaxmindGeoIPBundleInstalled(){
-     	$container = $this->getMockBuilder("Symfony\Component\DependencyInjection\ContainerInterface")->getMock();
-     	$container->expects($this->once())->method("hasParameter")->with('maxmind_geoip_data_file_path')->will($this->returnValue(__DIR__."/GeoLiteCity.dat"));
-     	$container->expects($this->once())->method("getParameter")->with('maxmind_geoip_data_file_path')->will($this->returnValue(__DIR__."/GeoLiteCity.dat"));
-     	$adapter = new MaxmindLookupAdapter($container);
-     	$country = $adapter->getCountry("8.8.8.8");
-     	$this->assertEquals("US", $country);
+     	if(class_exists("Maxmind\lib\GeoIp")){
+	     	$container = $this->getMockBuilder("Symfony\Component\DependencyInjection\ContainerInterface")->getMock();
+	     	$container->expects($this->once())->method("hasParameter")->with('maxmind_geoip_data_file_path')->will($this->returnValue(__DIR__."/GeoLiteCity.dat"));
+	     	$container->expects($this->once())->method("getParameter")->with('maxmind_geoip_data_file_path')->will($this->returnValue(__DIR__."/GeoLiteCity.dat"));
+	     	$adapter = new MaxmindLookupAdapter($container);
+	     	$country = $adapter->getCountry("8.8.8.8");
+	     	$this->assertEquals("US", $country);
+     	} else {
+			$this->markTestSkipped('The MaxMind library seems not to be installed/available.');
+     	}
      }
 }
